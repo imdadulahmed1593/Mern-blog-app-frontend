838f6e53c12fcdfaf4d7534abdfa92cb4ab0b0a6
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2023-01-02 14:30:45 +0600

    bug fixes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -19,13 +19,13 @@ export default function Sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
         <img
-          src="https://i.pinimg.com/236x/1e/3f/58/1e3f587572a7a7b20bbf1828595a1786--holiday-party-themes-holiday-gift-guide.jpg"
+          src="https://i.ibb.co/7nFchDR/dpo.jpg"
           alt=""
         />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui
-          necessitatibus nostrum illum reprehenderit.
-        </p>
+          This is a MERN stack blog app with full Users and Posts CRUD functionality.
+        You can register and login with your email and password. You can also post form the "write" tab after you log yourself in. You can edit or delete your post, but you can only see what others have posted (can't edit those)</p>
+        <p>Please have patience if the server takes some time to send the data (free server perks!)</p>
       </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIES</span>
